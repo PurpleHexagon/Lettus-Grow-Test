144aa764dbdad59ac2d668e02df7bafe0dc6d040
Jon Day <jon@plantigrate.com>
2020-02-07 17:46:08 +0000

    Add output device models

--- crop/admin.py
+++ crop/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Crop, GrowthPlan, Tray
+from .models import Crop, GrowthPlan, Tray, OutputDevice, OutputDeviceScheduledTask
 
 admin.site.register(Crop)
 admin.site.register(GrowthPlan)
 admin.site.register(Tray)
+admin.site.register(OutputDevice)
+admin.site.register(OutputDeviceScheduledTask)
--- crop/models.py
+++ crop/models.py
@@ -7,12 +7,30 @@ from django.core.validators import MaxLengthValidator, DecimalValidator
 
 class Crop(models.Model):
     """
-    A species of crop that can be grown in the farm
+    A species of crop that can be grown in the farm a
     """
 
     name = models.CharField(max_length=100, validators=[MaxLengthValidator(100)])  # name of crop
     family = models.CharField(max_length=100, validators=[MaxLengthValidator(100)])  # family of crop e.g. Basil
 
+class OutputDevice(models.Model):
+    """
+    An output device such as a grow light
+    """
+
+    device_type = models.IntegerField() # The type of the device represented by an integer, 1: Grow Light
+    units = models.IntegerField() # The units for the device
+    units_per_second = models.IntegerField() # Units per second outputted
+
+
+class OutputDeviceScheduledTask(models.Model):
+    """
+    A scheduled task for a device
+    """
+
+    output_device = models.ForeignKey(OutputDevice, on_delete=models.PROTECT)
+    start_time = models.DateTimeField(default=None, null=False) # The start time of the task
+    end_time = models.DateTimeField(default=None, null=True, blank=True) # The end time of the task
 
 class GrowthPlan(models.Model):
     """
@@ -23,6 +41,7 @@ class GrowthPlan(models.Model):
     name = models.CharField(max_length=50, validators=[MaxLengthValidator(50)])  # name of plan
     growth_duration = models.IntegerField()  # days
     est_yield = models.IntegerField()  # estimated yield grams
+    output_devices = models.ManyToManyField(OutputDevice)
 
 
 class Tray(models.Model):
